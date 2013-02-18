76894eca31c00d3fb62108f64b9beb6f748487ea
Christopher Trott <chris@twocentstudios.com>
2013-02-18 10:48:49 -0600

    View controller for viewing weekly chart time periods.

---
+++ vinylogue/TCSWeeklyChartViewController.h
@@ -0,0 +1,13 @@
+//
+//  TCSWeeklyChartViewController.h
+//  vinylogue
+//
+//  Created by Christopher Trott on 2/17/13.
+//  Copyright (c) 2013 TwoCentStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCSWeeklyChartViewController : UITableViewController
+
+@end
