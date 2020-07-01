500d71dde75b976d9d03984a2ec0ac4a40e2422a
Chris Trott <christopher-trott@cookpad.com>
2020-07-01 22:33:21 +0900

    empty project

---
+++ vinylogue/vinylogue/vinylogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  vinylogueApp.swift
+//  vinylogue
+//
+//  Created by christopher-trott on 2020/07/01.
+//
+
+import SwiftUI
+
+@main
+struct vinylogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
