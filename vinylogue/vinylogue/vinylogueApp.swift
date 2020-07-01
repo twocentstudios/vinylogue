ae94b747c6e6ef96d2694031418550c85d949ba9
Chris Trott <christopher-trott@cookpad.com>
2020-07-02 00:59:00 +0900

    configure app style

--- vinylogue/vinylogue/vinylogueApp.swift
+++ vinylogue/vinylogue/vinylogueApp.swift
@@ -5,6 +5,15 @@ struct vinylogueApp: App {
     var body: some Scene {
         WindowGroup {
             RootView()
+                .accentColor(.blueDark)
+                .onAppear {
+                    UINavigationBar.appearance().titleTextAttributes =
+                        [
+                            NSAttributedString.Key.font : UIFont(name: "AvenirNext-Regular", size: 20)!,
+                            NSAttributedString.Key.foregroundColor : UIColor(.blueDark)
+                        ]
+                    UINavigationBar.appearance().barTintColor = UIColor(.whiteSubtle)
+                }
         }
     }
 }
