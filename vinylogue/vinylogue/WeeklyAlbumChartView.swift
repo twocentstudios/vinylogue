4e078f7bce1a7768805cea71f09d531280dfcd91
Chris Trott <christopher-trott@cookpad.com>
2020-07-03 03:22:21 +0900

    navigation semantics for cell

--- vinylogue/vinylogue/WeeklyAlbumChartView.swift
+++ vinylogue/vinylogue/WeeklyAlbumChartView.swift
@@ -12,13 +12,30 @@ struct WeeklyAlbumChartView: View {
 
     let model: Model
 
+    @State var selectedItem: String?
+
     var body: some View {
         ScrollView {
             LazyVStack(spacing: 0, pinnedViews: [.sectionHeaders]) {
+                // TODO: real ids are required
                 ForEach(model.sections, id: \.label) { section in
                     Section(header: WeeklyAlbumChartHeaderView(label: section.label)) {
                         ForEach(section.albums, id: \.album) { album in
-                            WeeklyAlbumChartCell(model: album)
+                            VStack {
+                                NavigationLink(
+                                    destination: Text("Destination"),
+                                    isActive: Binding(get: {
+                                        self.selectedItem == album.album
+                                    }, set: { value in
+                                        self.selectedItem = value ? album.album : nil
+                                    })
+                                ) {
+                                    EmptyView()
+                                }
+                                WeeklyAlbumChartCell(album) {
+                                    self.selectedItem = album.album
+                                }
+                            }
                         }
                     }
                 }
@@ -74,6 +91,12 @@ struct WeeklyAlbumChartCell: View {
     }
 
     let model: Model
+    let action: () -> ()
+
+    init(_ model: Model, action: @escaping () -> () = {}) {
+        self.model = model
+        self.action = action
+    }
 
     private var imageView: Image {
         if let image = model.image {
@@ -84,81 +107,85 @@ struct WeeklyAlbumChartCell: View {
     }
 
     var body: some View {
-        VStack(spacing: 0) {
-            Rectangle()
-                .foregroundColor(.whitea(0.8))
-                .frame(height: 1)
-            HStack(spacing: 9) {
-                imageView
-                    .resizable()
-                    .frame(width: 80, height: 80, alignment: .center)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 2.0, style: .circular)
-                            .strokeBorder(Color.blacka(0.2), lineWidth: 1.0, antialiased: true)
-                    )
-                    .cornerRadius(2.0)
-                VStack {
-                    HStack {
-                        Text(model.artist.uppercased())
-                            .font(.avnUltraLight(12))
-                            .foregroundColor(.blueDark)
-                            .multilineTextAlignment(.leading)
+        Button(action: action) {
+            VStack(spacing: 0) {
+                Rectangle()
+                    .foregroundColor(.whitea(0.8))
+                    .frame(height: 1)
+                HStack(spacing: 9) {
+                    imageView
+                        .resizable()
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 2.0, style: .circular)
+                                .strokeBorder(Color.blacka(0.2), lineWidth: 1.0, antialiased: true)
+                        )
+                        .cornerRadius(2.0)
+                    VStack {
+                        HStack {
+                            Text(model.artist.uppercased())
+                                .font(.avnUltraLight(12))
+                                .foregroundColor(.blueDark)
+                                .multilineTextAlignment(.leading)
+                                .shadow(
+                                    color: .whitea(0.8),
+                                    radius: 1,
+                                    x: 0,
+                                    y: -0.5
+                                )
+                                .padding(.bottom, -1)
+                            Spacer()
+                        }
+                        HStack {
+                            Text(model.album)
+                                .font(.avnRegular(16))
+                                .foregroundColor(.blueDark)
+                                .multilineTextAlignment(.leading)
+                                .shadow(
+                                    color: .blacka(0.25),
+                                    radius: 1,
+                                    x: 0,
+                                    y: 0.5
+                                )
+                                .padding(.top, -1)
+                            Spacer()
+                        }
+                    }
+                    VStack {
+                        Text(model.plays)
+                            .font(.avnRegular(28))
+                            .foregroundColor(.blueBold)
+                            .lineLimit(1)
                             .shadow(
                                 color: .whitea(0.8),
                                 radius: 1,
                                 x: 0,
                                 y: -0.5
                             )
-                            .padding(.bottom, -1)
-                        Spacer()
+                            .padding(.bottom, -5)
+                        Text("plays")
+                            .font(.avnUltraLight(15))
+                            .foregroundColor(.gray(126))
+                            .lineLimit(1)
+                            .padding(.top, -5)
                     }
-                    HStack {
-                        Text(model.album)
-                            .font(.avnRegular(16))
-                            .foregroundColor(.blueDark)
-                            .multilineTextAlignment(.leading)
-                            .shadow(
-                                color: .blacka(0.25),
-                                radius: 1,
-                                x: 0,
-                                y: 0.5
-                            )
-                            .padding(.top, -1)
-                        Spacer()
-                    }
+                    .padding(.trailing, 2)
                 }
-                VStack {
-                    Text(model.plays)
-                        .font(.avnRegular(28))
-                        .foregroundColor(.blueBold)
-                        .lineLimit(1)
-                        .shadow(
-                            color: .whitea(0.8),
-                            radius: 1,
-                            x: 0,
-                            y: -0.5
-                        )
-                        .padding(.bottom, -5)
-                    Text("plays")
-                        .font(.avnUltraLight(15))
-                        .foregroundColor(.gray(126))
-                        .lineLimit(1)
-                        .padding(.top, -5)
-                }
-                .padding(.trailing, 2)
+                .padding(.horizontal, 12)
+                .padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                Rectangle()
+                    .foregroundColor(.blacka(0.1))
+                    .frame(height: 1)
             }
-            .padding(.horizontal, 12)
-            .padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            Rectangle()
-                .foregroundColor(.blacka(0.1))
-                .frame(height: 1)
+            .background(Color.whiteSubtle)
         }
+        .buttonStyle(PlainButtonStyle())
     }
 }
 
 struct WeeklyAlbumChartCell_Previews: PreviewProvider {
     static var previews: some View {
-        WeeklyAlbumChartCell(model: .init(image: nil, artist: "Weezer", album: "Maladroit", plays: "25"))
+        WeeklyAlbumChartCell(.init(image: nil, artist: "Weezer", album: "Maladroit", plays: "25"))
             .previewLayout(.sizeThatFits)
     }
 }
@@ -173,5 +200,4 @@ let mockAlbums: [WeeklyAlbumChartCell.Model] =
 let mockSections: [WeeklyAlbumChartView.Model.Section] =
     [
         .init(label: "2019", albums: mockAlbums),
-        .init(label: "2018", albums: mockAlbums),
     ]
