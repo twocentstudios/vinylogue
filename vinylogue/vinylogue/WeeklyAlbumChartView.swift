cd595341118823dadc71b51e18f79397578b54a7
Chris Trott <christopher-trott@cookpad.com>
2020-07-03 02:28:36 +0900

    weekly album tweaks

--- vinylogue/vinylogue/WeeklyAlbumChartView.swift
+++ vinylogue/vinylogue/WeeklyAlbumChartView.swift
@@ -1,16 +1,30 @@
 import SwiftUI
 
 struct WeeklyAlbumChartView: View {
+    struct Model {
+        struct Section {
+            let label: String
+            let albums: [WeeklyAlbumChartCell.Model]
+        }
+
+        let sections: [Section]
+    }
+
+    let model: Model
+
     var body: some View {
         ScrollView {
-            LazyVStack {
-                WeeklyAlbumChartHeaderView(label: "WEEK 27 of 2019")
-//                ForEach(friends, id: \.self) { friend in
-//                    SimpleCell(friend)
-//                }
+            LazyVStack(spacing: 0, pinnedViews: [.sectionHeaders]) {
+                ForEach(model.sections, id: \.label) { section in
+                    Section(header: WeeklyAlbumChartHeaderView(label: section.label)) {
+                        ForEach(section.albums, id: \.album) { album in
+                            WeeklyAlbumChartCell(model: album)
+                        }
+                    }
+                }
             }
         }
-        .navigationTitle("ybsc's charts")
+        .navigationTitle("ybsc's week 27 charts")
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarItems(trailing:
             ProgressView()
@@ -22,7 +36,7 @@ struct WeeklyAlbumChartView: View {
 struct WeeklyAlbumChartView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            WeeklyAlbumChartView()
+            WeeklyAlbumChartView(model: .init(sections: mockSections))
         }
     }
 }
@@ -37,15 +51,16 @@ struct WeeklyAlbumChartHeaderView: View {
                 .foregroundColor(.gray(120))
             Spacer()
         }
-        .padding(.vertical, 10)
-        .padding(.horizontal, 20)
+        .padding(.top, 12)
+        .padding(.bottom, 4)
+        .padding(.horizontal, 12)
         .background(Color.bluePeri)
     }
 }
 
 struct WeeklyAlbumChartHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        WeeklyAlbumChartHeaderView(label: "WEEK 27 of 2019")
+        WeeklyAlbumChartHeaderView(label: "2019")
             .previewLayout(.sizeThatFits)
     }
 }
@@ -69,67 +84,75 @@ struct WeeklyAlbumChartCell: View {
     }
 
     var body: some View {
-        HStack(spacing: 9) {
-            imageView
-                .resizable()
-                .frame(width: 80, height: 80, alignment: .center)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 2.0, style: .circular)
-                        .strokeBorder(Color.blacka(0.2), lineWidth: 1.0, antialiased: true)
-                )
-                .cornerRadius(2.0)
-            VStack {
-                HStack {
-                    Text(model.artist.uppercased())
-                        .font(.avnUltraLight(12))
-                        .foregroundColor(.blueDark)
-                        .multilineTextAlignment(.leading)
+        VStack(spacing: 0) {
+            Rectangle()
+                .foregroundColor(.whitea(0.8))
+                .frame(height: 1)
+            HStack(spacing: 9) {
+                imageView
+                    .resizable()
+                    .frame(width: 80, height: 80, alignment: .center)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 2.0, style: .circular)
+                            .strokeBorder(Color.blacka(0.2), lineWidth: 1.0, antialiased: true)
+                    )
+                    .cornerRadius(2.0)
+                VStack {
+                    HStack {
+                        Text(model.artist.uppercased())
+                            .font(.avnUltraLight(12))
+                            .foregroundColor(.blueDark)
+                            .multilineTextAlignment(.leading)
+                            .shadow(
+                                color: .whitea(0.8),
+                                radius: 1,
+                                x: 0,
+                                y: -0.5
+                            )
+                            .padding(.bottom, -1)
+                        Spacer()
+                    }
+                    HStack {
+                        Text(model.album)
+                            .font(.avnRegular(16))
+                            .foregroundColor(.blueDark)
+                            .multilineTextAlignment(.leading)
+                            .shadow(
+                                color: .blacka(0.25),
+                                radius: 1,
+                                x: 0,
+                                y: 0.5
+                            )
+                            .padding(.top, -1)
+                        Spacer()
+                    }
+                }
+                VStack {
+                    Text(model.plays)
+                        .font(.avnRegular(28))
+                        .foregroundColor(.blueBold)
+                        .lineLimit(1)
                         .shadow(
                             color: .whitea(0.8),
                             radius: 1,
                             x: 0,
                             y: -0.5
                         )
-                        .padding(.bottom, -1)
-                    Spacer()
+                        .padding(.bottom, -5)
+                    Text("plays")
+                        .font(.avnUltraLight(15))
+                        .foregroundColor(.gray(126))
+                        .lineLimit(1)
+                        .padding(.top, -5)
                 }
-                HStack {
-                    Text(model.album)
-                        .font(.avnRegular(16))
-                        .foregroundColor(.blueDark)
-                        .multilineTextAlignment(.leading)
-                        .shadow(
-                            color: .blacka(0.25),
-                            radius: 1,
-                            x: 0,
-                            y: 0.5
-                        )
-                        .padding(.top, -1)
-                    Spacer()
-                }
+                .padding(.trailing, 2)
             }
-            VStack {
-                Text(model.plays)
-                    .font(.avnRegular(28))
-                    .foregroundColor(.blueBold)
-                    .lineLimit(1)
-                    .shadow(
-                        color: .whitea(0.8),
-                        radius: 1,
-                        x: 0,
-                        y: -0.5
-                    )
-                    .padding(.bottom, -5)
-                Text("plays")
-                    .font(.avnUltraLight(15))
-                    .foregroundColor(.gray(126))
-                    .lineLimit(1)
-                    .padding(.top, -5)
-            }
-            .padding(.trailing, 2)
+            .padding(.horizontal, 12)
+            .padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            Rectangle()
+                .foregroundColor(.blacka(0.1))
+                .frame(height: 1)
         }
-        .padding(.horizontal, 12)
-        .padding(.vertical, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
     }
 }
 
@@ -139,3 +162,16 @@ struct WeeklyAlbumChartCell_Previews: PreviewProvider {
             .previewLayout(.sizeThatFits)
     }
 }
+
+let mockAlbums: [WeeklyAlbumChartCell.Model] =
+    [
+        .init(image: nil, artist: "Weezer", album: "Maladroit", plays: "20"),
+        .init(image: nil, artist: "Rufio", album: "The Comfort of Home", plays: "17"),
+        .init(image: nil, artist: "Saves The Day", album: "Sound The Alarm", plays: "2"),
+    ]
+
+let mockSections: [WeeklyAlbumChartView.Model.Section] =
+    [
+        .init(label: "2019", albums: mockAlbums),
+        .init(label: "2018", albums: mockAlbums),
+    ]
