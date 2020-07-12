2bb47f6a6abbd12b295768a25fa14629188bd637
Chris Trott <christopher-trott@cookpad.com>
2020-07-12 19:03:57 +0900

    remove some rootView overrides

--- vinylogue/vinylogue/vinylogueApp.swift
+++ vinylogue/vinylogue/vinylogueApp.swift
@@ -5,14 +5,11 @@ struct vinylogueApp: App {
     var body: some Scene {
         WindowGroup {
             RootView()
-                .accentColor(.blueDark)
                 .onAppear {
                     UINavigationBar.appearance().titleTextAttributes =
                         [
                             NSAttributedString.Key.font : UIFont(name: "AvenirNext-Regular", size: 20)!,
-                            NSAttributedString.Key.foregroundColor : UIColor(.blueDark)
                         ]
-                    UINavigationBar.appearance().barTintColor = UIColor(.whiteSubtle)
                 }
         }
     }
