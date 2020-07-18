4a234c1212e5fa60965a36d56928857d5a2df281
Chris Trott <christopher-trott@cookpad.com>
2020-07-19 01:03:25 +0900

    wire up favorite users view

--- vinylogue/vinylogue/FavoriteUsersView.swift
+++ vinylogue/vinylogue/FavoriteUsersView.swift
@@ -1,54 +1,76 @@
+import ComposableArchitecture
 import SwiftUI
 
 struct FavoriteUsersListView: View {
-    @State var me: String
-    @State var friends: [String]
+    struct State: Equatable {
+        let me: String
+        let friends: [String]
+        // TODO: add friends button / loading
+    }
+
+    let store: Store<FavoriteUsersState, FavoriteUsersAction>
 
     var body: some View {
-        List {
-            Section(header: SimpleHeader("me")) {
-                LargeSimpleCell(me)
+        WithViewStore(self.store.scope(state: \.view)) { viewStore in
+            List {
+                Section(header: SimpleHeader("me")) {
+                    LargeSimpleCell(viewStore.me)
+                }
+                Section(
+                    header: SimpleHeader("friends")
+                ) {
+                    ForEach(viewStore.friends, id: \.self) { friend in
+                        SimpleCell(friend)
+                    }
+                    .onDelete { indexSet in
+                        print(indexSet)
+                    }
+                    .onMove { indecies, newOffset in
+                        print(indecies)
+                    }
+                }
             }
-            Section(
-                header: SimpleHeader("friends")
-            ) {
-                ForEach(friends, id: \.self) { friend in
-                    SimpleCell(friend)
-                }
-                .onDelete { indexSet in
-                    print(indexSet)
-                }
-                .onMove { indecies, newOffset in
-                    print(indecies)
-                }
-            }
+            .listStyle(GroupedListStyle())
+            .navigationTitle("scrobblers")
+            .navigationBarItems(
+                leading: Button {
+                    viewStore.send(.didTapSettings)
+                    print("settings")
+                } label: {
+                    Image("settings")
+                        .renderingMode(.original)
+                },
+                trailing: EditButton()
+            )
         }
-        .listStyle(GroupedListStyle())
-        .navigationTitle("scrobblers")
-        .navigationBarItems(
-            leading: Button {
-                print("settings")
-            } label: {
-                Image("settings")
-                    .renderingMode(.original)
-            },
-            trailing: EditButton()
-        )
     }
 }
 
-struct FavoriteUsersListView_Previews: PreviewProvider {
+ struct FavoriteUsersListView_Previews: PreviewProvider {
     static let me = "ybsc"
     static let friends = ["BobbyStompy", "slippydrums", "esheikh"]
+    static var store: Store<FavoriteUsersState, FavoriteUsersAction> = {
+        Store(
+            initialState: FavoriteUsersState(user: .mock),
+            reducer: favoriteUsersReducer,
+            environment: .mockFirstTime
+        )
+    }()
     static var previews: some View {
         Group {
             NavigationView {
-                FavoriteUsersListView(me: me, friends: friends)
+                FavoriteUsersListView(store: store)
             }
             .preferredColorScheme(.dark)
             NavigationView {
-                FavoriteUsersListView(me: me, friends: friends)
+                FavoriteUsersListView(store: store)
             }
         }
+    }
+ }
+
+extension FavoriteUsersState {
+    var view: FavoriteUsersListView.State {
+        .init(me: user.me, friends: user.friends)
     }
 }
