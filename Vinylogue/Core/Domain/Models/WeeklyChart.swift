45e88a10cfc9718ee86acde8a6d2ed3b917cae6f
Christopher Trott <chris@twocentstudios.com>
2025-06-22 17:02:57 +0900

    remove unused getters

--- Vinylogue/Core/Domain/Models/WeeklyChart.swift
+++ Vinylogue/Core/Domain/Models/WeeklyChart.swift
@@ -5,14 +5,6 @@ struct WeeklyChart: Codable, Hashable, Sendable {
     let to: Date
     let albums: [UserChartAlbum]
 
-    var weekNumber: Int {
-        Calendar.current.component(.weekOfYear, from: from)
-    }
-
-    var year: Int {
-        Calendar.current.component(.year, from: from)
-    }
-
     init(from: Date, to: Date, albums: [UserChartAlbum] = []) {
         self.from = from
         self.to = to
